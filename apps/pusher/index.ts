76c7601a81c2f675f6f768c36706d198de41d958
Vikas Prasad <vikas.prasad@globalgarner.com>
2026-01-09 18:50:00 +0530

    backend complete

---
+++ apps/pusher/index.ts
@@ -0,0 +1,22 @@
+import { prismaClient } from "store/client";
+import { xAddBulk } from "redisstream/client";
+
+async function main() {
+  let websites = await prismaClient.website.findMany({
+    select: {
+      url: true,
+      id: true,
+    },
+  });
+  console.log("Fetched websites:", websites.length);
+  await xAddBulk(websites.map((w) => ({ url: w.url, id: w.id })));
+}
+
+setInterval(
+  () => {
+    main();
+  },
+  3 * 1000 * 60
+); 
+
+main();
